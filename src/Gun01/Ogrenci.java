249530fc898b5c055a4257a4c083b1f3305c032e
corejohn <schreibankorcan@yahoo.de>
2023-01-30 20:43:10 +0300

    task bitti

--- src/Gun01/Ogrenci.java
+++ src/Gun01/Ogrenci.java
@@ -4,6 +4,7 @@ public class Ogrenci {
     public static void main(String[] args) {
         System.out.println("üzüm iyidir");
         System.out.println("merhaba");
+        System.out.println("üzüm çeşitleri");
 
 
     }
